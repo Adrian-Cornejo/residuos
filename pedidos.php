983aacb48a36ebcf152c5a61e7a29b02f98533fd
ANGEL\Angel RD <angel2gol@hotmail.com>
2024-10-04 21:10:29 -0600

    creacion de el apartado de pedidos

---
+++ pedidos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pedidos</title>
+    <link rel="stylesheet" href="styles/stylesPedidos.css">
+</head>
+<body>
+    <header class="header-container">
+        <div class="back-btn" onclick="window.history.back()"><- Regresar</div>
+        <div class="logo-container" >
+            <img src="logo.png" alt="Logo de la empresa" class="logo">
+            <h1>Alimenta Hoy</h1>
+        </div>
+    </header>
+
+    <main>
+
+    <section>
+        <h2>Pedidos</h2>
+
+
+
+        <!- imputs ->
+        <label>Nombre:</label>
+        <input type="text" id=input placeholder="Ingrese su nombre" onfocus="this.placeholder= ''"  onblur="this.placeholder ='Ingrese su nombre'" >
+
+        <!-- Primera lista desplegable -->
+        <label for="localidad">Seleccionar Localidad:</label>
+            <select id="localidad" name="Localidad">
+                <option value="">Cargando Localidades...</option>
+            </select>
+
+            
+
+    </section>
+
+
+
+    </main>
+    
+
+    <!-- Pie de página -->
+    <footer>
+        <p>&copy; 2024 Mi Empresa. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
